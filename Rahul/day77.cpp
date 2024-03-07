bbf406a9c2b3f8b3b8faad2491749af38f9c8a10
Rahulrai888 <rahulrai1010101@gmail.com>
2024-03-07 17:48:50 +0530

    days

---
+++ Rahul/day77.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+          ListNode* slow = head;
+        ListNode* fast = head->next;
+        while(fast != NULL){
+            fast = fast->next;
+            if(fast != NULL)
+                fast = fast->next;
+         slow = slow->next;
+        }
+
+     return slow;   
+    }
+};

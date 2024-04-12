f73688b50573e58dc4c6ae1932089608c04b8dac
Shubham Kumar <114337784+shubh1777@users.noreply.github.com>
2024-04-12 20:23:55 +0530

    Day103

---
+++ Shubham/Day103-lc-42. Trapping Rain Water.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int trap(int[] height) {
+        int n= height.length;
+        int[] left=new int[n];
+        int[] right =new int[n];
+        left[0]=height[0];
+        for(int i=1;i<n;i++ ){
+            left[i]=Math.max(left[i-1],height[i]);
+        }
+        right[n-1]=height[n-1];
+        for(int j=n-2;j>=0;j--){
+            right[j]=Math.max(right[j+1],height[j]);
+        }
+        int ans=0;
+        for(int k=0;k<n;k++){
+            ans=ans+(Math.min(left[k],right[k])-height[k]);
+        }
+        return ans;
+    }
+}

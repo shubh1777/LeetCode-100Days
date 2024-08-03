10b4afcf766be246c3532453d7bed70fab2362c0
Rahulrai888 <rahulrai1010101@gmail.com>
2024-08-03 16:07:39 +0530

    days

---
+++ Rahul/day215.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool canBeEqual(vector<int>& target, vector<int>& arr) {
+        sort(target.begin(),target.end());
+        sort(arr.begin(),arr.end());
+        for(int i=0;i<target.size();i++){
+            if(target[i] == arr[i]){
+                continue;
+            }else{
+                return false;
+            }
+        }
+        return true;
+    }
+};
